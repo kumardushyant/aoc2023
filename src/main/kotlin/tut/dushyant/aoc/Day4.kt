97859dd85d49b4c6681e76a59803ae3d81e00df1
Dushyant Kumar <dushyant.nimbekar@soprabanking.com>
2023-12-10 17:26:44 +0530

    Day 4 solved

--- src/main/kotlin/tut/dushyant/aoc/Day4.kt
+++ src/main/kotlin/tut/dushyant/aoc/Day4.kt
@@ -2,6 +2,7 @@ package tut.dushyant.aoc
 
 import kotlin.math.pow
 
+
 data class Card (val id:Int, val leftArray: List<Int>, val rightArray: List<Int>) {
 
     override fun toString(): String {
@@ -9,7 +10,7 @@ data class Card (val id:Int, val leftArray: List<Int>, val rightArray: List<Int>
     }
 }
 
-fun getCards(): List<Card> = Common.readInputAsArray("testday4.txt").map { row ->
+fun getCards(): List<Card> = Common.readInputAsArray("day4.txt").map { row ->
     val (idPart, winArray, myArray) = row.joinToString(separator = "").split(":", " | ")
     Card(idPart.substring(4).trim().toInt(),
         winArray.chunked(3).map { it.trim().toInt() },
@@ -19,17 +20,23 @@ fun getCards(): List<Card> = Common.readInputAsArray("testday4.txt").map { row -
 fun main() {
 
     //part 1
-    /*println(getCards().sumOf { card ->
+    println(getCards().sumOf { card ->
         2.0.pow( card.leftArray.count { it in card.rightArray }-1).toInt()
-    })*/
+    })
 
     //part 2
     //map of card to found
     getCards().map { card ->
         val found:Int = card.leftArray.count { it in card.rightArray }
         card to found
-    }.reversed().fold(emptyList<Int>()) { acc, (card,found) ->
-        listOf(1 + (0..found).sumOf { acc[card.id] })+acc
+    }.reversed().fold(emptyList<Int>()) { acc, (_,found) ->
+        val sum = 1 + (0..<found).sumOf {
+            if(acc.isEmpty())
+                0
+            else
+                acc[it]
+        }
+        listOf(sum)+acc
     }.sum().also(::println)
 
 }
